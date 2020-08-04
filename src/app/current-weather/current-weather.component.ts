ce02d07d7c26927bf4acb7e9d1b5ff0e5ca2c705
Walid Bouguima <walid.bouguima@gmail.com>
2020-08-04 14:33:56 +0100

    Add a component and interface for current weather and implement the selector in tha app.component.html

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css'],
+})
+export class CurrentWeatherComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
