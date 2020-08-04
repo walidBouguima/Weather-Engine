b83f846ab298b78fb7f35ae20dcefcbed3677668
Walid Bouguima <walid.bouguima@gmail.com>
2020-08-04 17:31:16 +0100

    Finish the method responsible of the data fetching from the api

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+
+import { environment } from '../../environments/environment'
 
 interface ICurrentWeatherData {
   weather: [
@@ -23,4 +25,25 @@ interface ICurrentWeatherData {
 })
 export class WeatherService {
   constructor(private httpClient: HttpClient) {}
+
+  // tslint:disable-next-line: typedef
+  // hard coded complicated string url
+  /* getCurrentWeather(city: string, country: string) {
+    return this.httpClient.get<ICurrentWeatherData>(
+      `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?` +
+        `q=${city},${country}&appid=${environment.appId}`
+    )
+  }*/
+  // use HttParams to build the URL programmatically
+  // tslint:disable-next-line: typedef
+  getCurrentWeather(city: string, country: string) {
+    const uriParams = new HttpParams()
+      .set('q', `${city}, ${country}`)
+      .set('appid', environment.appId)
+
+    return this.httpClient.get<ICurrentWeatherData>(
+      `${environment.baseUrl}api.openweathermap.or/data/2.5/weather`,
+      { params: uriParams }
+    )
+  }
 }
