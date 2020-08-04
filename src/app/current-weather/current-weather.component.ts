be3f604838409548cbadd3d7d683ed7f2aa591bc
Walid Bouguima <walid.bouguima@gmail.com>
2020-08-04 15:29:46 +0100

    Wired up the current weather component

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core'
 
+import { ICurrentWeather } from '../interfaces'
+
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css'],
 })
 export class CurrentWeatherComponent implements OnInit {
-  constructor() {}
+  current: ICurrentWeather
+
+  constructor() {
+    this.current = {
+      city: 'Tunis',
+      country: 'TN',
+      date: new Date(),
+      image: 'assets/img/sun.png',
+      temperature: 44,
+
+      description: 'sunny',
+    } as ICurrentWeather
+  }
 
   ngOnInit(): void {}
 }
