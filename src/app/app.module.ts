b96c274ad82c1901d92c8f1e2e0d7620a530a610
Walid Bouguima <walid.bouguima@gmail.com>
2020-08-05 22:06:37 +0100

    Fix prettier and install Javascript Standard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 import { CurrentWeatherComponent } from './current-weather/current-weather.component'
 
 @NgModule({
